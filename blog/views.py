6443d1288d99675c6dda3584784a34ed1932f8e8
mot3 <hopdrmot3@gmail.com>
2021-05-30 19:24:23 +0430

    preceding code blog.views.list_view to PostListView

--- blog/views.py
+++ blog/views.py
@@ -1,27 +1,36 @@
-from django.core import paginator
-from django.shortcuts import render, get_object_or_404
+from django.views.generic import ListView, DetailView
 from .models import Post
-from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+
+class PostListView(ListView):
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog/post/list.html'
+
+# from django.core import paginator
+from django.shortcuts import render, get_object_or_404
+# from .models import Post
+# from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 
-def post_list(request):
-    # Get all post from published manager
-    # that return those posts status choises is publish.
-    posts = Post.published.all()
+# def post_list(request):
+#     # Get all post from published manager
+#     # that return those posts status choises is publish.
+#     posts = Post.published.all()
 
-    ########## Page paginator ##########
-    paginator = Paginator(posts, 3)  # n post in each page
-    page = request.GET.get('page') # Get page from url
+#     ########## Page paginator ##########
+#     paginator = Paginator(posts, 3)  # n post in each page
+#     page = request.GET.get('page') # Get page from url
 
-    try:
-        posts = paginator.page(page) # Go to that page
-    except PageNotAnInteger:
-        # If page is not an integer deliver the first page
-        posts = paginator.page(paginator.num_pages)
-    ########## End page paginator ##########
+#     try:
+#         posts = paginator.page(page) # Go to that page
+#     except PageNotAnInteger:
+#         # If page is not an integer deliver the first page
+#         posts = paginator.page(paginator.num_pages)
+#     ########## End page paginator ##########
 
-    # And return a httpResponse to the page with these modle, and html template
-    return render(request, 'blog/post/list.html', {'posts': posts})
+#     # And return a httpResponse to the page with these modle, and html template
+#     return render(request, 'blog/post/list.html', {'posts': posts})
 
 
 def post_detail(request, year, month, day, post):
